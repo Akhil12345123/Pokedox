44f5317b2934d41f528c4b7bde6622d1ceae2290
Akhil <akhilbcoder@gmail.com>
2024-07-30 11:34:49 +0530

    installing React in Pokemon React

---
+++ src/Components/Pokemon.tsx
@@ -0,0 +1,11 @@
+
+const Pokemon = () => {
+  return (
+    <div>
+      <h1>Lets Catch the Pokemon</h1>
+      <input placeholder="Search here " />
+    </div>
+  )
+}
+
+export default Pokemon
---
+++ src/Components/Pokemon_Card.tsx
@@ -0,0 +1,10 @@
+
+const Pokemon_Card = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Pokemon_Card
